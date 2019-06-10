62e4d1e3dcbba87d5f6a2ebac8ffb797f1848e16
Phillip Yen <yphillip@gmail.com>
2019-06-09 21:36:22 -0700

    added closures.py

---
+++ closures.py
@@ -0,0 +1,29 @@
+def outer_func(msg):
+    message = msg 
+
+    def inner_func():
+        """Note that this inner function oes not take in any arguments"""
+        print(message)
+
+    return inner_func
+
+
+def main():
+    hi_func = outer_func('hi')
+    hello_func = outer_func('hello')
+    print(hi_func.__name__)
+    print(hello_func.__name__)
+
+    # A closure is an inner function that remembers and has access to variables
+    # in the local scope in which the variable was created, even after the outer
+    # function has finished executing.
+
+    # Each of these functions remembers the values of its own msg variable.
+    # A closure closes over the free variables from its environment
+    # (in this case, message).
+    hi_func()
+    hello_func()
+
+
+if __name__ == "__main__":
+    main()
